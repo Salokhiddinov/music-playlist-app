2980553e08026a71d2e54681786165428bc902d1
Nomonjon <nomonsalakhiddinov2002@gmail.com>
2022-03-21 21:08:29 +0500

    feat: start server

---
+++ server.js
@@ -0,0 +1,39 @@
+// Importing packages
+const express = require("express");
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const path = require('path');
+
+// App
+const app = express();
+
+// PORT
+const port = process.env.PORT || 8000;
+dotenv.config({
+    path: "config.env"
+})
+
+// Requests
+app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({extended:true}))
+
+// Engine
+app.set('view engine', "ejs");
+
+// Paths
+app.use('/css', express.static(path.resolve(__dirname, 'assets/css')))
+app.use('/img', express.static(path.resolve(__dirname, 'assets/img')))
+app.use('/js', express.static(path.resolve(__dirname, 'assets/js')))
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Music Playlist Application");
+});
+
+app.listen(port, () => {
+  console.log(`Server is on port ${port} 🤘`);
+  console.log(`Link: http://localhost:${port}`)
+});
