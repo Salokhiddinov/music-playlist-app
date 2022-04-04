6449cdca220f1155cc287a26c61156f2b99e8388
Salokhiddinov <nomonsalakhiddinov2002@gmail.com>
2022-04-05 00:26:35 +0500

    feat: create welcome screen

--- server/services/render.js
+++ server/services/render.js
@@ -1,12 +1,11 @@
-const axios = require("axios");
 const Song = require("../model/model");
 
 exports.homeRoutes = (req, res) => {
   res.render("index");
 };
-exports.not_found_page = (req,res)=>{
-  res.render('not-found.ejs')
-}
+exports.not_found_page = (req, res) => {
+  res.render("not-found.ejs");
+};
 
 exports.all_songs_page = async (req, res) => {
   const songs = await Song.find({});
@@ -23,14 +22,13 @@ exports.get_song_page = async (req, res) => {
 };
 
 exports.update_song_page = async (req, res) => {
-  try{
+  try {
     const songData = await Song.findByIdAndUpdate({ _id: req.query.id });
     res.render("update-song.ejs", { song: songData });
-  }
-  catch(error){
+  } catch (error) {
     res.status(404).send({
-      status: 'failed',
-      message: error
-    })
+      status: "failed",
+      message: error,
+    });
   }
 };
